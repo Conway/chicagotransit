2aa684dad9bc7248eb4774ee7420ea5b4f5a1f0c
Jake Conway <jake.h.conway@gmail.com>
2016-09-28 16:54:50 +0000

    More bugfixes

--- setup.py
+++ setup.py
@@ -6,12 +6,9 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-with open(path.join(here, 'requirements.txt')) as fi:
-    requirements = fi.read().splitlines()
-
 setup(
     name='chicagotransit',
-    version='0.4.1',
+    version='0.4.2',
     description='A package for interfacing with Chicago Transit APIs',
     long_description=long_description,
     url='https://github.com/conway/ChicagoTransit',
@@ -31,5 +28,5 @@ setup(
     ],
     keywords=['chicago', 'transit', 'wrapper', 'bus', 'train', 'cta', 'divvy', 'illinois', 'IL', 'transportation'],
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    install_requires=requirements,
+    install_requires=['requests', 'xmltodict==0.10.2']
 )
